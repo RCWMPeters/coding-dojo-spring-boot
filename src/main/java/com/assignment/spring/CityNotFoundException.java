b11c34d4dc2efee398f2e39cc9cda97204bbf4f6
RCWMPeters <r.c.w.m.peters@gmail.com>
2020-07-07 14:19:08 +0200

    Removed solution

--- src/main/java/com/assignment/spring/CityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.spring;
-
-public class CityNotFoundException extends RuntimeException {
-    public CityNotFoundException(String city) {
-        super("Could not find city " + city);
-    }
-}
-
